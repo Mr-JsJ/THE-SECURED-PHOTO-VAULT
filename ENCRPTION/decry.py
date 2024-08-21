b197623bf44e027c7276d5fa73a4a682085b8f56
JITHIN S JOHN <116088672+Mr-JsJ@users.noreply.github.com>
2024-08-22 03:16:58 +0530

    encrpton and decription

--- ENCRPTION/decry.py
+++ ENCRPTION/decry.py
@@ -1,5 +1,4 @@
 import os
-import json
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -7,52 +6,51 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 
-# Specify the directory containing the encrypted images
-image_folder = "path_to_your_folder"
+# Load the private key for decryption
+private_key_path = 'C:/Users/jsjji/OneDrive/Desktop/git/MCA-MINI PROJECT/THE-SECURED-PHOTO-VAULT/ENCRPTION/private_key.pem'
+with open(private_key_path, 'rb') as f:
+    private_key = serialization.load_pem_private_key(
+        f.read(),
+        password=None,
+        backend=default_backend()
+    )
 
-# Load the private key
-with open('private_key.pem', 'rb') as f:
-    private_key = serialization.load_pem_private_key(f.read(), password=None, backend=default_backend())
+# Load the public key for decryption
+public_key_path = 'C:/Users/jsjji/OneDrive/Desktop/git/MCA-MINI PROJECT/THE-SECURED-PHOTO-VAULT/ENCRPTION/public_key.pem'
+with open(public_key_path, 'rb') as f:
+    public_key = serialization.load_pem_public_key(
+        f.read(),
+        backend=default_backend()
+    )
 
-# Load the keys dictionary from the JSON file
-json_file_path = os.path.join(image_folder, 'encryption_keys.json')
-with open(json_file_path, 'r') as json_file:
-    keysofimage = json.load(json_file)
+# Load the encrypted image
+encrypted_image_path = 'C:/Users/jsjji/OneDrive/Desktop/git/MCA-MINI PROJECT/THE-SECURED-PHOTO-VAULT/ENCRPTION/EIMAGES/encrypted_image.bin'
+with open(encrypted_image_path, 'rb') as f:
+    salt = f.read(16)
+    iv = f.read(16)
+    ciphertext = f.read()
 
-# Process each encrypted image in the folder
-for filename in os.listdir(image_folder):
-    if filename.startswith('encrypted_') and filename.endswith('.bin'):
-        encrypted_image_path = os.path.join(image_folder, filename)
-        original_image_name = filename[len('encrypted_'):-4]  # Extract the original image name
+# Derive the symmetric key from the shared key
+shared_key = private_key.exchange(ec.ECDH(), public_key)
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
+symmetric_key = kdf.derive(shared_key)
 
-        # Load the encrypted image
-        with open(encrypted_image_path, 'rb') as f:
-            salt = f.read(16)
-            iv = f.read(16)
-            ciphertext = f.read()
+# Decrypt the image using AES
+cipher = Cipher(algorithms.AES(symmetric_key), modes.CBC(iv), backend=default_backend())
+decryptor = cipher.decryptor()
 
-        # Retrieve the corresponding symmetric key from the JSON file
-        symmetric_key_hex = keysofimage.get(original_image_name)
-        if symmetric_key_hex is None:
-            print(f"No key found for {original_image_name}")
-            continue
+# Remove padding after decryption
+padded_image_data = decryptor.update(ciphertext) + decryptor.finalize()
+unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+image_data = unpadder.update(padded_image_data) + unpadder.finalize()
 
-        symmetric_key = bytes.fromhex(symmetric_key_hex)
-
-        # Decrypt the image using AES
-        cipher = Cipher(algorithms.AES(symmetric_key), modes.CBC(iv), backend=default_backend())
-        decryptor = cipher.decryptor()
-
-        # Remove padding after decryption
-        padded_image_data = decryptor.update(ciphertext) + decryptor.finalize()
-        unpadder = padding.PKCS7(128).unpadder()
-        image_data = unpadder.update(padded_image_data) + unpadder.finalize()
-
-        # Save the decrypted image
-        decrypted_image_path = os.path.join(image_folder, f"decrypted_{original_image_name}")
-        with open(decrypted_image_path, 'wb') as f:
-            f.write(image_data)
-
-        print(f"Decrypted and saved: {decrypted_image_path}")
-
-print("Decryption complete.")
+# Save the decrypted image
+decrypted_image_path = 'C:/Users/jsjji/OneDrive/Desktop/git/MCA-MINI PROJECT/THE-SECURED-PHOTO-VAULT/ENCRPTION/DIMAGES/decrypted_image.png'
+with open(decrypted_image_path, 'wb') as f:
+    f.write(image_data)
--- ENCRPTION/encry.py
+++ ENCRPTION/encry.py
@@ -1,15 +1,10 @@
 import os
-import json
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
-
-# Specify the directory containing the images
-image_folder = "IMAGES"
-keysofimage = {}
 
 # Generate ECC key pair
 private_key = ec.generate_private_key(ec.SECP384R1(), default_backend())
@@ -21,62 +16,51 @@ public_key_bytes = public_key.public_bytes(
     format=serialization.PublicFormat.SubjectPublicKeyInfo
 )
 
-# Process each image in the folder
-for filename in os.listdir(image_folder):
-    if filename.endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif', '.tiff')):
-        image_path = os.path.join(image_folder, filename)
+# Load the image to encrypt
+image_path = 'C:/Users/jsjji/OneDrive/Desktop/git/MCA-MINI PROJECT/THE-SECURED-PHOTO-VAULT/ENCRPTION/apple.jpg'
+with open(image_path, 'rb') as f:
+    image_data = f.read()
 
-        # Load the image to encrypt
-        with open(image_path, 'rb') as f:
-            image_data = f.read()
+# Generate a shared key using ECC
+shared_key = private_key.exchange(ec.ECDH(), public_key)
 
-        # Generate a shared key using ECC
-        shared_key = private_key.exchange(ec.ECDH(), public_key)
+# Derive a symmetric key from the shared key
+salt = os.urandom(16)
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
+symmetric_key = kdf.derive(shared_key)
 
-        # Derive a symmetric key from the shared key
-        salt = os.urandom(16)
-        kdf = PBKDF2HMAC(
-            algorithm=hashes.SHA256(),
-            length=32,
-            salt=salt,
-            iterations=100000,
-            backend=default_backend()
-        )
-        symmetric_key = kdf.derive(shared_key)
+# Encrypt the image using AES
+iv = os.urandom(16)
+cipher = Cipher(algorithms.AES(symmetric_key), modes.CBC(iv), backend=default_backend())
+encryptor = cipher.encryptor()
 
-        # Encrypt the image using AES
-        iv = os.urandom(16)
-        cipher = Cipher(algorithms.AES(symmetric_key), modes.CBC(iv), backend=default_backend())
-        encryptor = cipher.encryptor()
+# Padding the image data to be a multiple of the block size
+padder = padding.PKCS7(algorithms.AES.block_size).padder()
+padded_image_data = padder.update(image_data) + padder.finalize()
 
-        # Padding the image data to be a multiple of the block size
-        padder = padding.PKCS7(128).padder()
-        padded_image_data = padder.update(image_data) + padder.finalize()
+ciphertext = encryptor.update(padded_image_data) + encryptor.finalize()
 
-        ciphertext = encryptor.update(padded_image_data) + encryptor.finalize()
-
-        # Save the encrypted image
-        encrypted_image_path = os.path.join(image_folder, f"encrypted_{filename}.bin")
-        with open(encrypted_image_path, 'wb') as f:
-            f.write(salt + iv + ciphertext)
-
-        # Store the derived symmetric key in the dictionary
-        keysofimage[filename] = symmetric_key.hex()
+# Save the encrypted image
+encrypted_image_path = 'C:/Users/jsjji/OneDrive/Desktop/git/MCA-MINI PROJECT/THE-SECURED-PHOTO-VAULT/ENCRPTION/EIMAGES/encrypted_image.bin'
+with open(encrypted_image_path, 'wb') as f:
+    f.write(salt + iv + ciphertext)
 
 # Save the private key and public key
-with open('private_key.pem', 'wb') as f:
+private_key_path = 'C:/Users/jsjji/OneDrive/Desktop/git/MCA-MINI PROJECT/THE-SECURED-PHOTO-VAULT/ENCRPTION/private_key.pem'
+public_key_path = 'C:/Users/jsjji/OneDrive/Desktop/git/MCA-MINI PROJECT/THE-SECURED-PHOTO-VAULT/ENCRPTION/public_key.pem'
+
+with open(private_key_path, 'wb') as f:
     f.write(private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
         encryption_algorithm=serialization.NoEncryption()
     ))
 
-with open('public_key.pem', 'wb') as f:
+with open(public_key_path, 'wb') as f:
     f.write(public_key_bytes)
-
-# Save the keys dictionary to a JSON file
-json_file_path = os.path.join(image_folder, 'encryption_keys.json')
-with open(json_file_path, 'w') as json_file:
-    json.dump(keysofimage, json_file, indent=4)
-
-print(f"Encryption complete. Keys stored in: {json_file_path}")
